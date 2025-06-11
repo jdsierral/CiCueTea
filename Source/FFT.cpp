b8c627a11496e2eb126153f70df3f748837742e9
JuanS <juans@JuanSaudio.com>
2025-06-11 18:37:12 +0400

    Added FFTSelection structure

--- Source/FFT.cpp
+++ Source/FFT.cpp
@@ -7,9 +7,22 @@
 
 #include "FFT.hpp"
 
+#ifdef FFT_FFTW
 #include "FFT_FFTW.h"
-// #include "FFT_vDSP.h"
-// #include "FFT_PFFFT.h"
+#endif
+
+#ifdef FFT_PFFFT
+#include "FFT_PFFFT.h"
+#endif
+
+#ifdef FFT_VDSP
+#include "FFT_vDSP.h"
+#endif
+
+#ifdef FFT_MKL
+#include "FFT_MKL.h"
+#endif
+
 
 using namespace jsa;
 using namespace Eigen;
@@ -17,43 +30,43 @@ using namespace Eigen;
 DFT::DFT(size_t fftSize) : pImpl(new DFTImpl(fftSize)) {}
 
 void DFT::dft(const ArrayXcd &X, ArrayXcd &Y) {
-  pImpl->dft(X.data(), Y.data());
+    pImpl->dft(X.data(), Y.data());
 }
 
 void DFT::idft(const ArrayXcd &X, ArrayXcd &Y) {
-  pImpl->idft(X.data(), Y.data());
+    pImpl->idft(X.data(), Y.data());
 }
 
 void DFT::rdft(const ArrayXd &x, ArrayXcd &X) {
-  pImpl->rdft(x.data(), X.data());
+    pImpl->rdft(x.data(), X.data());
 }
 
 void DFT::irdft(const ArrayXcd &X, ArrayXd &x) {
-  pImpl->irdft(X.data(), x.data());
+    pImpl->irdft(X.data(), x.data());
 }
 
 //==========================================================================
 
 void DFT::dft(const ArrayXXcd &X, ArrayXXcd &Y) {
-  for (Index k = 0; k < X.cols(); k++) {
-    pImpl->dft(X.col(k).data(), Y.col(k).data());
-  }
+    for (Index k = 0; k < X.cols(); k++) {
+        pImpl->dft(X.col(k).data(), Y.col(k).data());
+    }
 }
 
 void DFT::idft(const ArrayXXcd &X, ArrayXXcd &Y) {
-  for (Index k = 0; k < X.cols(); k++) {
-    pImpl->idft(X.col(k).data(), Y.col(k).data());
-  }
+    for (Index k = 0; k < X.cols(); k++) {
+        pImpl->idft(X.col(k).data(), Y.col(k).data());
+    }
 }
 
 void DFT::rdft(const ArrayXXd &x, ArrayXXcd &X) {
-  for (Index k = 0; k < x.cols(); k++) {
-    pImpl->rdft(x.col(k).data(), X.col(k).data());
-  }
+    for (Index k = 0; k < x.cols(); k++) {
+        pImpl->rdft(x.col(k).data(), X.col(k).data());
+    }
 }
 
 void DFT::irdft(const ArrayXXcd &X, ArrayXXd &x) {
-  for (Index k = 0; k < X.cols(); k++) {
-    pImpl->irdft(X.col(k).data(), x.col(k).data());
-  }
+    for (Index k = 0; k < X.cols(); k++) {
+        pImpl->irdft(X.col(k).data(), x.col(k).data());
+    }
 }
--- Source/FFT_vDSP.h
+++ Source/FFT_vDSP.h
@@ -15,70 +15,64 @@ using namespace Eigen;
 namespace jsa {
 class DFTImpl {
 public:
-  DFTImpl() {}
-
-  ~DFTImpl() { cleanup(); }
-
-  void init(size_t fftSize) {
-    cleanup();
-    this->fftSize = fftSize;
-
-    r2cSetup = vDSP_DFT_Interleaved_CreateSetupD(
-        r2cSetup, fftSize / 2, vDSP_DFT_FORWARD,
-        vDSP_DFT_Interleaved_RealtoComplex);
-    c2rSetup = vDSP_DFT_Interleaved_CreateSetupD(
-        c2rSetup, fftSize / 2, vDSP_DFT_INVERSE,
-        vDSP_DFT_Interleaved_RealtoComplex);
-    c2cSetup = vDSP_DFT_Interleaved_CreateSetupD(
-        c2cSetup, fftSize, vDSP_DFT_FORWARD,
-        vDSP_DFT_Interleaved_ComplextoComplex);
-    ic2cSetup = vDSP_DFT_Interleaved_CreateSetupD(
-        ic2cSetup, fftSize, vDSP_DFT_INVERSE,
-        vDSP_DFT_Interleaved_ComplextoComplex);
-  }
-
-  void dft(const dcomplex* inPtr, dcomplex* outPtr) {
-    DSPDoubleComplex* inPtr_ =
+    DFTImpl(size_t fftSize) : fftSize(fftSize) {
+        r2cSetup = vDSP_DFT_Interleaved_CreateSetupD(
+                                                     r2cSetup, fftSize / 2, vDSP_DFT_FORWARD,
+                                                     vDSP_DFT_Interleaved_RealtoComplex);
+        c2rSetup = vDSP_DFT_Interleaved_CreateSetupD(
+                                                     c2rSetup, fftSize / 2, vDSP_DFT_INVERSE,
+                                                     vDSP_DFT_Interleaved_RealtoComplex);
+        c2cSetup = vDSP_DFT_Interleaved_CreateSetupD(
+                                                     c2cSetup, fftSize, vDSP_DFT_FORWARD,
+                                                     vDSP_DFT_Interleaved_ComplextoComplex);
+        ic2cSetup = vDSP_DFT_Interleaved_CreateSetupD(
+                                                      ic2cSetup, fftSize, vDSP_DFT_INVERSE,
+                                                      vDSP_DFT_Interleaved_ComplextoComplex);
+    }
+    
+    ~DFTImpl() {
+        if (r2cSetup) vDSP_DFT_Interleaved_DestroySetupD(r2cSetup);
+        if (c2rSetup) vDSP_DFT_Interleaved_DestroySetupD(c2rSetup);
+        if (c2cSetup) vDSP_DFT_Interleaved_DestroySetupD(c2cSetup);
+        if (ic2cSetup) vDSP_DFT_Interleaved_DestroySetupD(ic2cSetup);
+    }
+    
+    void dft(const dcomplex* inPtr, dcomplex* outPtr) {
+        DSPDoubleComplex* inPtr_ =
         reinterpret_cast<DSPDoubleComplex*>(const_cast<dcomplex*>(inPtr));
-    DSPDoubleComplex* outPtr_ = reinterpret_cast<DSPDoubleComplex*>(outPtr);
-    vDSP_DFT_Interleaved_ExecuteD(c2cSetup, inPtr_, outPtr_);
-  }
-
-  void idft(const dcomplex *inPtr, dcomplex *outPtr) {
-    DSPDoubleComplex* inPtr_ =
+        DSPDoubleComplex* outPtr_ = reinterpret_cast<DSPDoubleComplex*>(outPtr);
+        vDSP_DFT_Interleaved_ExecuteD(c2cSetup, inPtr_, outPtr_);
+    }
+    
+    void idft(const dcomplex *inPtr, dcomplex *outPtr) {
+        DSPDoubleComplex* inPtr_ =
         reinterpret_cast<DSPDoubleComplex*>(const_cast<dcomplex*>(inPtr));
-    DSPDoubleComplex* outPtr_ = reinterpret_cast<DSPDoubleComplex*>(outPtr);
-    vDSP_DFT_Interleaved_ExecuteD(ic2cSetup, inPtr_, outPtr_);
-    Map<ArrayXcd>(outPtr, fftSize) *= (1.0 / fftSize);
-  }
-
-  void rdft(const double* inPtr, dcomplex* outPtr) {
-    DSPDoubleComplex* inPtr_ =
+        DSPDoubleComplex* outPtr_ = reinterpret_cast<DSPDoubleComplex*>(outPtr);
+        vDSP_DFT_Interleaved_ExecuteD(ic2cSetup, inPtr_, outPtr_);
+        Map<ArrayXcd>(outPtr, fftSize) *= (1.0 / fftSize);
+    }
+    
+    void rdft(const double* inPtr, dcomplex* outPtr) {
+        DSPDoubleComplex* inPtr_ =
         reinterpret_cast<DSPDoubleComplex*>(const_cast<double*>(inPtr));
-    DSPDoubleComplex* outPtr_ = reinterpret_cast<DSPDoubleComplex*>(outPtr);
-    vDSP_DFT_Interleaved_ExecuteD(r2cSetup, inPtr_, outPtr_);
-    Map<ArrayXcd>(outPtr, fftSize) *= (0.5);
-  }
-
-  void irdft(const dcomplex* inPtr, double* outPtr) {
-    DSPDoubleComplex* inPtr_ =
+        DSPDoubleComplex* outPtr_ = reinterpret_cast<DSPDoubleComplex*>(outPtr);
+        vDSP_DFT_Interleaved_ExecuteD(r2cSetup, inPtr_, outPtr_);
+        Map<ArrayXcd>(outPtr, fftSize/2+1) *= (0.5);
+    }
+    
+    void irdft(const dcomplex* inPtr, double* outPtr) {
+        DSPDoubleComplex* inPtr_ =
         reinterpret_cast<DSPDoubleComplex*>(const_cast<dcomplex*>(inPtr));
-    DSPDoubleComplex* outPtr_ = reinterpret_cast<DSPDoubleComplex*>(outPtr);
-    vDSP_DFT_Interleaved_ExecuteD(c2rSetup, inPtr_, outPtr_);
-  }
-
+        DSPDoubleComplex* outPtr_ = reinterpret_cast<DSPDoubleComplex*>(outPtr);
+        vDSP_DFT_Interleaved_ExecuteD(c2rSetup, inPtr_, outPtr_);
+        Map<ArrayXd>(outPtr, fftSize) *= (1.0 / fftSize);
+    }
+    
 private:
-  void cleanup() {
-    vDSP_DFT_Interleaved_DestroySetupD(r2cSetup);
-    vDSP_DFT_Interleaved_DestroySetupD(c2rSetup);
-    vDSP_DFT_Interleaved_DestroySetupD(c2cSetup);
-    vDSP_DFT_Interleaved_DestroySetupD(ic2cSetup);
-  }
-
-  size_t fftSize;
-  vDSP_DFT_Interleaved_SetupD r2cSetup = nullptr;
-  vDSP_DFT_Interleaved_SetupD c2rSetup = nullptr;
-  vDSP_DFT_Interleaved_SetupD c2cSetup = nullptr;
-  vDSP_DFT_Interleaved_SetupD ic2cSetup = nullptr;
+    const size_t fftSize;
+    vDSP_DFT_Interleaved_SetupD r2cSetup = nullptr;
+    vDSP_DFT_Interleaved_SetupD c2rSetup = nullptr;
+    vDSP_DFT_Interleaved_SetupD c2cSetup = nullptr;
+    vDSP_DFT_Interleaved_SetupD ic2cSetup = nullptr;
 };
 } // namespace jsa
